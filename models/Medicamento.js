3523125e2d6eca7a964de12601c1d2855b7af4d2
Jeisson Stewen <75818097+jeissonstewen@users.noreply.github.com>
2024-05-29 23:27:24 -0500

    Update Medicamento.js

--- models/Medicamento.js
+++ models/Medicamento.js
@@ -1,18 +1,50 @@
+// const mongoose = require('../DB/ConectionDB');
+
+// const Medicamento = mongoose.Schema(
+//     {
+//         "codigoMedicamento": {
+//             "type": "string",
+//             "required": true
+//         },
+//         "nombreMedicamento": {
+//             "type": "string",
+//             "required": true
+//         },
+//         "descripcionMedicamento": {
+//             "type": "string",
+//             "required": true
+//         }
+//     },
+//     {
+//         collection: "Medicamentos",
+//         versionKey: false
+//     }
+// );
+
+// module.exports = mongoose.model("Medicamento", Medicamento);
 const mongoose = require('../DB/ConectionDB');
 
-const Medicamento = mongoose.Schema(
+const MedicamentoSchema = new mongoose.Schema(
     {
-        "codigoMedicamento": {
-            "type": "string",
-            "required": true
+        codigo: {
+            type: String,
+            required: true
         },
-        "nombreMedicamento": {
-            "type": "string",
-            "required": true
+        nombre: {
+            type: String,
+            required: true
         },
-        "descripcionMedicamento": {
-            "type": "string",
-            "required": true
+        laboratorio: {
+            type: String,
+            required: true
+        },
+        componenteActivo: {
+            type: String,
+            required: true
+        },
+        descripcionMedicamento: {
+            type: String,
+            required: true
         }
     },
     {
@@ -21,4 +53,4 @@ const Medicamento = mongoose.Schema(
     }
 );
 
-module.exports = mongoose.model("Medicamento", Medicamento);
+module.exports = mongoose.model("Medicamento", MedicamentoSchema);

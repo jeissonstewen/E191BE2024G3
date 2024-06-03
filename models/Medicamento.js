0155be40ec35a23f95c5f0ce09841ed787d960d5
Jeisson Stewen <75818097+jeissonstewen@users.noreply.github.com>
2024-06-03 02:51:59 -0500

    Update Medicamento.js

--- models/Medicamento.js
+++ models/Medicamento.js
@@ -1,27 +1,3 @@
-// const mongoose = require('../DB/ConectionDB');
-
-// const Medicamento = mongoose.Schema(
-//     {
-//         "codigoMedicamento": {
-//             "type": "string",
-//             "required": true
-//         },
-//         "nombreMedicamento": {
-//             "type": "string",
-//             "required": true
-//         },
-//         "descripcionMedicamento": {
-//             "type": "string",
-//             "required": true
-//         }
-//     },
-//     {
-//         collection: "Medicamentos",
-//         versionKey: false
-//     }
-// );
-
-// module.exports = mongoose.model("Medicamento", Medicamento);
 const mongoose = require('../DB/ConectionDB');
 
 const MedicamentoSchema = new mongoose.Schema(
